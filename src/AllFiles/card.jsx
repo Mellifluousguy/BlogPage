430c1abe709931ab18e4d3053e2e881e609aa662
Mohit Anilkumar Gupta <123147288+Mellifluousguy@users.noreply.github.com>
2025-05-18 20:54:33 +0530

    added hover on images

--- src/AllFiles/card.jsx
+++ src/AllFiles/card.jsx
@@ -13,13 +13,13 @@ const card = ({ blog, index }) => {
             whileInView={"show"}
             viewport={{ once: true, amount: 0.7 }}
             key={blog.title} className='flex py-4 relative flex-col cursor-pointer  gap-1' >
-            <img src={blog.image} alt={blog.title} className='w-80 shadow-lg h-48 object-cover rounded-xl' />
+            <img src={blog.image} alt={blog.title} className='w-80 shadow-lg h-48 object-cover rounded-xl hover:scale-105' />
             <span className='text-sm text-gray-800'>{blog.readTime}</span>
             <h2 className='text-lg '>{blog.title}</h2>
             <p className='text-gray-600 text-sm'>{blog.subtitle.slice(0, 44)}</p>
 
             <div className=' flex gap-2 items-center relative text-xs'>
-                <img src="https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg" alt={blog.name} className='w-[35px] object-cover rounded-full h-[35px]' />
+                <img src="https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg" alt={blog.name} className='w-[35px]  object-cover rounded-full h-[35px]' />
                 <div>
                     <span className='text-gray-600'>Writer</span>
                     <p>{blog.author}</p>
